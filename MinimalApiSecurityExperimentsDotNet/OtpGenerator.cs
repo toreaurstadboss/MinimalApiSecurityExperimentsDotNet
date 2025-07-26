6aa1a0b3aae67a71085f31f17bf869e0fb62941b
Tore Aurstad <tore.aurstad@gmail.com>
2025-07-26 23:33:24 +0200

    Init commit

---
+++ MinimalApiSecurityExperimentsDotNet/OtpGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MinimalApiSecurityExperimentsDotNet
+{
+    
+    public static class OtpGenerator
+    {
+
+        /// <summary>
+        /// Generates an OTP = One Time Password. Just a random number of given <paramref name="length"/>
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string GenerateOtp(int length)
+        {
+            var builder = new StringBuilder();
+            for (var i = 0; i < length; i++)
+            {
+                builder.Append(RandomNumberGenerator.GetInt32(0, 10));
+            }
+            return builder.ToString();
+        }
+
+    }
+
+}
---
+++ MinimalApiSecurityExperimentsDotNet/Program.cs
@@ -0,0 +1,36 @@
+namespace MinimalApiSecurityExperimentsDotNet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+
+            app.MapGet("/otp", () =>
+            {
+                return OtpGenerator.GenerateOtp(10);
+
+            });
+
+            app.Run();
+        }
+    }
+
+
+}
