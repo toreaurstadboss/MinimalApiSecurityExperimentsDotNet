2e6537791283fd424d588bbb695dafed213ae40d
Tore Aurstad <tore.aurstad@gmail.com>
2025-07-26 23:43:41 +0200

    Added two demos to the repo

---
+++ MinimalApiSecurityExperimentsDotNet/GuidGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace MinimalApiSecurityExperimentsDotNet
+{
+    
+    public static class GuidGenerator
+    {
+
+        public static Guid CreateCryptographicallySecureGuid()
+        {
+            var data = RandomNumberGenerator.GetBytes(16);
+            return new Guid(data);
+
+        }
+
+    }
+
+}
--- MinimalApiSecurityExperimentsDotNet/Program.cs
+++ MinimalApiSecurityExperimentsDotNet/Program.cs
@@ -21,11 +21,29 @@ namespace MinimalApiSecurityExperimentsDotNet
 
             app.MapControllers();
 
+            app.MapGet("/guid", () =>
+            {
+                return GuidGenerator.CreateCryptographicallySecureGuid();
+            });
+
+            app.MapGet("/", () =>
+            {
+                string html = """
+                <h2>Welcome to the Security demos !</h2>
+                <ul>
+                    <li><a href="/guid">Generate a cryptographically secure GUID</a></li>
+                    <li><a href="/otp">Generate a One Time Password (OTP)</a></li> 
+                </ul>
+                """;
+
+
+                return Results.Content(html, "text/html");
+            });
+
 
             app.MapGet("/otp", () =>
             {
                 return OtpGenerator.GenerateOtp(10);
-
             });
 
             app.Run();
