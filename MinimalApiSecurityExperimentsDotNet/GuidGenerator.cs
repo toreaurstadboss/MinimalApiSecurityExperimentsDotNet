7063b8689be52d30f04c72c3ba5fd93b78a63201
Tore Aurstad <tore.aurstad@gmail.com>
2025-07-27 00:00:07 +0200

    Added cryptographically strong Guid UUID v4

--- MinimalApiSecurityExperimentsDotNet/GuidGenerator.cs
+++ MinimalApiSecurityExperimentsDotNet/GuidGenerator.cs
@@ -6,9 +6,21 @@ namespace MinimalApiSecurityExperimentsDotNet
     public static class GuidGenerator
     {
 
+        /// <summary>
+        /// Creates a RFC 4122-compliant UUIDv4 generator
+        /// </summary>
+        /// <returns></returns>
         public static Guid CreateCryptographicallySecureGuid()
         {
             var data = RandomNumberGenerator.GetBytes(16);
+
+            // Set the version to 4 (UUIDv4)
+            data[6] = (byte)((data[6] & 0x0F) | 0x40);
+
+            // Set the variant to RFC 4122
+            data[8] = (byte)((data[8] & 0x3F) | 0x80);
+
+
             return new Guid(data);
 
         }
